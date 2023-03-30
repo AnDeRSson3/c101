2cbc681abfa1467f43df14531e619ea88e6abb24
AnDeRSson3 <103232122+AnDeRSson3@users.noreply.github.com>
2023-03-30 12:00:16 -0700

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,41 @@
+import random
+respon="y"
+while 1==1:
+     
+        if respon=='y':
+
+
+            
+            no = random.randint(1,6)
+            if no==1:
+                print("[-----]")
+                print("[-----]")
+                print("[--0--]")
+                print("[-----]")
+                print("[-----]")
+            if no==2:
+                print("[-----]")
+                print("[---0-]")
+                print("[-----]")
+                print("[-0---]")
+                print("[-----]")
+            if no==3:
+                print("[-----]")
+                print("[-----]")
+                print("[0-0-0]")
+                print("[-----]")
+                print("[-----]")
+            if no==4:
+                print("[-----]")
+                print("[-0-0-]")
+                print("[-----]")
+                print("[-0-0-]")
+                print("[-----]")
+            if no==5:
+                print("[-----]")
+                print("[-0-0-]")
+                print("[--0--]")
+                print("[-0-0-]")
+                print("[-----]")
+respon=input("Do you want to roll a dice?:")
+   
